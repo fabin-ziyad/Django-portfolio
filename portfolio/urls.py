6799dd4ffe08d829465d38940e1323c8871af65f
Fabin Ziyad <83161513+fabin-ziyad@users.noreply.github.com>
2022-08-08 14:48:11 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.static import serve
+from django.conf.url import url
 admin.site.site_header = 'Fabin Ziyad [ Admin Panel ]'
 urlpatterns = [
     path('', include('feeds.urls')),
     path('ownerlogin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
